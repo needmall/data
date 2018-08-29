8a7dbdad7146dc80f64f9c70d55b9509f77f6309
trustray <trustray7@gmail.com>
2018-08-29 21:29:37 +0900

    dd

--- nzsproject/src/main/java/com/needmall/seller/productRequest/controller/ProductRequestController.java
+++ nzsproject/src/main/java/com/needmall/seller/productRequest/controller/ProductRequestController.java
@@ -1,8 +1,13 @@
 package com.needmall.seller.productRequest.controller;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.needmall.common.vo.ReqproductVO;
+import com.needmall.seller.productRequest.service.ProductRequestService;
 
 
 @Controller
@@ -10,5 +15,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ProductRequestController {
 	Logger logger = Logger.getLogger(ProductRequestController.class);
 	
-
+	@Autowired
+	private ProductRequestService productRequestService;
+	
+	public String productRequstUpdate(ReqproductVO rvo, Model model) {
+		logger.info("productRequstUpdate  호출 성공");
+		int result=0;
+		result=productRequestService.ProductRequestUpdate(rvo);
+		model.addAttribute("result", result);
+		return "seller/productrequest/productRequstUpdate";
+	}
 }
