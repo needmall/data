3613eb9d1945b063fd13baa3c2fdcebf0a4295a6
trustray <trustray7@gmail.com>
2018-08-30 11:11:07 +0900

    d

--- nzsproject/src/main/java/com/needmall/admin/product/vo/ReqproductRegistVO.java
+++ nzsproject/src/main/java/com/needmall/admin/product/vo/ReqproductRegistVO.java
@@ -3,5 +3,27 @@ package com.needmall.admin.product.vo;
 import com.needmall.common.vo.ReqproductVO;
 
 public class ReqproductRegistVO extends ReqproductVO{
-
+	private int c1_num;
+	private String c1_name;
+	private String c2_name;
+	public int getC1_num() {
+		return c1_num;
+	}
+	public void setC1_num(int c1_num) {
+		this.c1_num = c1_num;
+	}
+	public String getC1_name() {
+		return c1_name;
+	}
+	public void setC1_name(String c1_name) {
+		this.c1_name = c1_name;
+	}
+	public String getC2_name() {
+		return c2_name;
+	}
+	public void setC2_name(String c2_name) {
+		this.c2_name = c2_name;
+	}
+	
+	
 }
