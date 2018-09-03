f6b04ac51656761fc3fb351690c046acaeeb11b1
trustray <trustray7@gmail.com>
2018-09-03 11:06:40 +0900

    mybatis 수정 강기복

--- nzsproject/src/main/java/com/needmall/admin/statistic/dao/StatisticDaoImpl.java
+++ nzsproject/src/main/java/com/needmall/admin/statistic/dao/StatisticDaoImpl.java
@@ -6,11 +6,18 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.needmall.admin.statistic.vo.SalesDataVO;
+
 @Repository
 public class StatisticDaoImpl implements StatisticDao{
 	
 	@Autowired
 	private SqlSession session;
+
+	@Override
+	public List<SalesDataVO> totalData() {
+		return session.selectList("totalData");
+	}
 
 		
 	
--- nzsproject/src/main/java/com/needmall/admin/statistic/service/StatisticServiceImpl.java
+++ nzsproject/src/main/java/com/needmall/admin/statistic/service/StatisticServiceImpl.java
@@ -1,10 +1,13 @@
 package com.needmall.admin.statistic.service;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.needmall.admin.statistic.dao.StatisticDao;
+import com.needmall.admin.statistic.vo.SalesDataVO;
 
 
 @Service
@@ -12,7 +15,13 @@ public class StatisticServiceImpl implements StatisticService{
 
 	
 	@Autowired
-	private StatisticDao statisticRegistDao;
+	private StatisticDao statisticDao;
+
+	@Override
+	public List<SalesDataVO> totalData() {
+		List<SalesDataVO> list = statisticDao.totalData();
+		return list;
+	}
 	
 	
 			
